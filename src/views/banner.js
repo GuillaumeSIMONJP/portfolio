df54ea2aeff9a1b4c390194739a4a2bcbb159a79
Guillaume Simon <simon.guillaume.j.p@gmail.com>
2024-10-09 03:39:38 +0200

    add (view) : adding banner.js

---
+++ src/views/banner.js
@@ -0,0 +1,18 @@
+export default () => (`
+<div
+  class="bg-indigo-600 px-4 py-3 text-white sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+>
+  <p class="text-center font-medium sm:text-left">
+    À la recherche d'un alternant en développement web?
+    <br class="sm:hidden" />
+     N'hésiter pas à me contacter pour plus d'informations
+  </p>
+
+  <a
+    class="mt-4 block rounded-full bg-white px-5 py-3 text-center text-sm font-medium text-indigo-600 transition hover:bg-white/90 focus:outline-none focus:ring active:text-indigo-500 sm:mt-0"
+    href="#contacts"
+  >
+    Contacts
+  </a>
+</div>
+`);
